513a79854f7eb5775e9a7944c793ad3f96906528
Ike Ahmed <ikeahmed@IAHMEDMAX.local>
2024-08-21 21:30:50 -0500

    for loop

---
+++ src/com/me/forloop/ForLoop.java
@@ -0,0 +1,11 @@
+package com.me.forloop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++)
+                System.out.println("solveProblem");
+            System.out.println("sharpenPencil");
+        }
+    }
+}
